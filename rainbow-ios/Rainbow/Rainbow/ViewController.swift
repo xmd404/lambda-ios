9a87507fe60613f6308e9acda24339d930059fdc
Xavier Duncan <xduncan@pm.me>
2019-04-26 14:07:53 -0400

    init rainbow-ios app

---
+++ rainbow-ios/Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Xavier Duncan on 4/26/19.
+//  Copyright © 2019 Xavier Duncan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
